08a1e0fd31d84216506ea6fb444de9d3f5672274
BoBoBalloon <61363161+BoBoBalloon@users.noreply.github.com>
2021-06-18 11:25:20 -0400

    added damage percent keyword

--- src/main/java/me/boboballoon/innovativeitems/InnovativeItems.java
+++ src/main/java/me/boboballoon/innovativeitems/InnovativeItems.java
@@ -44,7 +44,7 @@ public final class InnovativeItems extends JavaPlugin {
 
         this.keywordManager.registerKeywords(new DelayKeyword(), new DamageKeyword(), new HealKeyword(), new ParticleKeyword(),
                 new MessageKeyword(), new EffectKeyword(), new AbilityKeyword(), new CommandKeyword(),
-                new RandomAbilityKeyword());
+                new RandomAbilityKeyword(), new DamagePercentKeyword());
     }
 
     @Override
---
+++ src/main/java/me/boboballoon/innovativeitems/keywords/builtin/DamagePercentKeyword.java
@@ -0,0 +1,93 @@
+package me.boboballoon.innovativeitems.keywords.builtin;
+
+import com.google.common.collect.ImmutableList;
+import me.boboballoon.innovativeitems.keywords.context.DamageContext;
+import me.boboballoon.innovativeitems.keywords.context.RuntimeContext;
+import me.boboballoon.innovativeitems.keywords.keyword.Keyword;
+import me.boboballoon.innovativeitems.keywords.keyword.KeywordContext;
+import me.boboballoon.innovativeitems.keywords.keyword.KeywordTargeter;
+import me.boboballoon.innovativeitems.util.LogUtil;
+import org.bukkit.entity.LivingEntity;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
+/**
+ * Class that represents a keyword in an ability config file that damages by percent of max health a selected target
+ */
+public class DamagePercentKeyword extends Keyword {
+    public DamagePercentKeyword() {
+        super("damagepercent", true, false);
+    }
+
+    @Override
+    public void execute(List<Object> arguments, RuntimeContext context) {
+        LivingEntity target = null;
+        KeywordTargeter rawTarget = (KeywordTargeter) arguments.get(0);
+
+        if (rawTarget == KeywordTargeter.PLAYER) {
+            target = context.getPlayer();
+        }
+
+        if (context instanceof DamageContext && rawTarget == KeywordTargeter.ENTITY) {
+            DamageContext damageContext = (DamageContext) context;
+            target = damageContext.getEntity();
+        }
+
+        if (target == null) {
+            LogUtil.log(Level.WARNING, "There is not a valid living entity currently present on the " + this.getIdentifier() + " keyword on the " + context.getAbilityName() + " ability! Are you sure the target and trigger are valid together?");
+            return;
+        }
+
+        double percent = (Double) arguments.get(1);
+
+        double amount = target.getMaxHealth() * percent;
+
+        target.damage(amount);
+    }
+
+    @Override
+    @Nullable
+    public List<Object> load(KeywordContext context) {
+        String[] raw = context.getContext();
+        List<Object> args = new ArrayList<>();
+
+        KeywordTargeter rawTarget = KeywordTargeter.getFromIdentifier(raw[0]);
+
+        if (rawTarget == null) {
+            LogUtil.log(Level.WARNING, "There is not a valid target entered on the " + this.getIdentifier() + " keyword on the " + context.getAbilityName() + " ability!");
+            return null;
+        }
+
+        if (rawTarget != KeywordTargeter.PLAYER && rawTarget != KeywordTargeter.ENTITY) {
+            LogUtil.log(Level.WARNING, "There is not a valid target entered on the " + this.getIdentifier() + " keyword on the " + context.getAbilityName() + " ability!");
+            return null;
+        }
+
+        args.add(rawTarget);
+
+        double percent;
+        try {
+            percent = Double.parseDouble(raw[1]);
+        } catch (NumberFormatException e) {
+            LogUtil.log(Level.WARNING, "There is not a valid damage entered on the " + this.getIdentifier() + " keyword on the " + context.getAbilityName() + " ability!");
+            return null;
+        }
+
+        args.add(percent / 100); //convert to decimal
+
+        return args;
+    }
+
+    @Override
+    public ImmutableList<String> getValidTargeters() {
+        return ImmutableList.of("?player", "?entity");
+    }
+
+    @Override
+    public boolean isAsync() {
+        return false;
+    }
+}
